ac4aaa76d2261c6c4e8dc5146bcc43220456c22d
Yujin Kim <yujii22001@gmail.com>
2026-02-04 23:00:48 +0900

    3.13 HttpServletRequest

---
+++ src/main/java/hello/servlet/basic/request/RequestHeaderServlet.java
@@ -0,0 +1,54 @@
+package hello.servlet.basic.request;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.Enumeration;
+
+@WebServlet(name="requestHeaderServlet", urlPatterns = "/request-header")
+public class RequestHeaderServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        printStartLine(request);
+        printHeaders(request);
+    }
+
+    //
+    private void printStartLine(HttpServletRequest request){
+        System.out.println("--- REQUEST-LINE - start ---");
+
+        System.out.println("request.getMethod() = " + request.getMethod());
+        System.out.println("request.getProtocol() = " + request.getProtocol());
+        System.out.println("request.getScheme() = " + request.getScheme());
+
+        // http://localhost:8080/request-header
+        System.out.println("request.getRequestURL() = " + request.getRequestURL());
+
+        // /request-test
+        System.out.println("request.getRequestURI() = " + request.getRequestURI());
+
+        // username=hello
+        System.out.println("request.getQueryString() = " + request.getQueryString());
+        System.out.println("request.isSecure() = " + request.isSecure());
+        System.out.println("--- REQUEST-LINE - end ---");
+        System.out.println();
+    }
+    
+    // Header 모든 정보
+    private void printHeaders(HttpServletRequest request){
+        System.out.println("--- Headers - start ---");
+
+        Enumeration<String> headerNames = request.getHeaderNames();
+        while (headerNames.hasMoreElements()){
+            String headerName = headerNames.nextElement();
+            System.out.println("headerName = " + headerName);
+        }
+
+        System.out.println("--- Headers - end ---");
+        System.out.println();
+    }
+}
